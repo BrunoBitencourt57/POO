37a6221a655c5a325179f47cc68b18ca029c4f51
Brayan Jone <brayanjone01@gmail.com>
2026-04-14 20:19:22 -0300

    Update Usuario.java

--- src/Usuario.java
+++ src/Usuario.java
@@ -3,7 +3,24 @@ public class Usuario {
     private String nome;
     private String senha;
     private String email;
-    private String local;
-    private date nascimento;
-    private int carteira;
+    private int nascimento;
+    private Carteira carteira;
+    private Biblioteca biblioteca;
+
+    public Usuario(String nome, String senha, String email, int nascimento){
+        this.nome = nome;
+        this.senha = password;
+        this.email = email;
+        this.nascimento = nascimento;
+        this.biblioteca = new Biblioteca();
+        this.carteira = new Carteira();
+    }
+        
+    public void comprarJogo(Jogo jogo, Loja loja){
+       loja.venderJogo(this, jogo);
+    }
+    
+    public void listarBiblioteca(){
+       biblioteca.listarJogos();
+   }
 }
