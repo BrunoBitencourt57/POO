a073c553586d439402c3cad20ff0d76ae27b2706
Brayan Jone <brayanjone01@gmail.com>
2026-04-23 16:48:32 -0300

    Update Loja.java

--- src/Loja.java
+++ src/Loja.java
@@ -2,8 +2,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Loja {
+    
+    private List<Jogo> jogosDisponiveis = new ArrayList<>();
 
-    private List<Jogo> jogo = new ArrayList<>();
+    public void venderJogo(Usuario usuario, Jogo jogo) {
 
-    public venderJogo
+        if (!jogosDisponiveis.contains(jogo)) {
+            System.out.println("Jogo não disponível!");
+            return;
+        }
+
+        if (usuario.getCarteira().debitar(jogo.getPreco())) {
+            usuario.getBiblioteca().adicionarJogo(jogo);
+
+            Compra compra = new Compra(usuario, jogo); 
+            compra.exibirCompra();
+
+            System.out.println("Compra realizada!");
+        } else {
+            System.out.println("Saldo insuficiente!");
+        }
+    }
 }
