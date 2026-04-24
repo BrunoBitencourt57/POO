3c65e72e5b534233d3c98d5ca8c3d88728287ee8
Brayan Jone <brayanjone01@gmail.com>
2026-04-23 21:18:25 -0300

    Update Avaliacao.java

--- src/Avaliacao.java
+++ src/Avaliacao.java
@@ -1,7 +1,53 @@
+import java.time.LocalDateTime;
+
 public class Avaliacao {
 
     private Usuario usuario;
     private Jogo jogo;
     private int nota;
     private String comentario;
+    private LocalDateTime data;
+
+    public Avaliacao(Usuario usuario, Jogo jogo, int nota, String comentario) {
+        this.usuario = usuario;
+        this.jogo = jogo;
+        setNota(nota); // validação
+        this.comentario = comentario;
+        this.data = LocalDateTime.now();
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public Jogo getJogo() {
+        return jogo;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setNota(int nota) {
+        if (nota < 1 || nota > 5) {
+            throw new IllegalArgumentException("Nota deve ser entre 1 e 5");
+        }
+        this.nota = nota;
+    }
+
+    @Override
+    public String toString() {
+        return "Jogo: " + jogo.getNome() +
+               " | Nota: " + nota +
+               " | Comentário: " + comentario +
+               " | Usuário: " + usuario.getNome();
+    }
 }
