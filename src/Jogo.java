a0303284c0399394c4eef3b031b7fcf7f473d44b
Brayan Jone <brayanjone01@gmail.com>
2026-04-23 21:26:15 -0300

    Update Jogo.java

--- src/Jogo.java
+++ src/Jogo.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Jogo {
     private String nome;
     private double preco;
+    private List<Avaliacao> avaliacoes = new ArrayList<>();
 
     public Jogo(String nome, double preco) {
         this.nome = nome;
@@ -8,10 +12,36 @@ public class Jogo {
     }
 
     public String getNome() {
-        return nome = nome;
+        return nome;
     }
 
     public double getPreco() {
         return preco;
+    }
+
+    public void adicionarAvaliacao(Avaliacao avaliacao) {
+    for (Avaliacao a : avaliacoes) {
+        if (a.getUsuario().equals(avaliacao.getUsuario())) {
+            System.out.println("Usuário já avaliou esse jogo!");
+            return;
+        }
+    }
+    avaliacoes.add(avaliacao);
+}
+
+    public double getMediaAvaliacoes() {
+        if (avaliacoes.isEmpty()) return 0;
+    
+        double soma = 0;
+        for (Avaliacao a : avaliacoes) {
+            soma += a.getNota();
+        }
+    
+        return soma / avaliacoes.size();
+    }
+
+    @Override
+    public String toString() {
+        return nome + " - R$ " + preco + " | Média: " + getMediaAvaliacoes();
     }
 }
