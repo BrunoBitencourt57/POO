33ba56f202e5b58641f767c022893b36fde09fd1
adrianagnolin <adrianaagnolin.aluno@unipampa.edu.br>
2026-04-28 17:34:07 -0300

    Update PagamentoCarteira.java

--- src/PagamentoCarteira.java
+++ src/PagamentoCarteira.java
@@ -10,12 +10,10 @@ public class PagamentoCarteira implements FormaDePagamento {
     public boolean pagar(double valor) {
         if (carteira == null) {
             throw new IllegalArgumentException("Carteira inválida!");
-            return false;
         }
 
         if (valor <= 0) {
             throw new IllegalArgumentException("Valor inválido!");
-            return false;
         }
 
         return carteira.debitar(valor);
