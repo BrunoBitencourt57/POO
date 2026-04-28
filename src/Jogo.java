3b396dcf213d1ebcf2d96604dc6858f643ec13ef
BrunoBitencourt57 <brunomoura.aluno@unipampa.edu.br>
2026-04-28 16:20:31 -0300

    Update Jogo.java

--- src/Jogo.java
+++ src/Jogo.java
@@ -10,29 +10,38 @@ public class Jogo extends Produto {
         super(nome, preco);
     }
 
-     public void adicionarDLC(DLC dlc) {
-          if (dlc != null) {
-              dlcs.add(dlc);
-          }
-     } 
+    public void adicionarDLC(DLC dlc) {
+        if (dlc == null) return;
+
+        if (!dlc.getJogoBase().equals(this)) {
+            System.out.println("DLC não pertence a este jogo!");
+            return;
+        }
+
+        dlcs.add(dlc);
+    } 
 
     public void adicionarAvaliacao(Avaliacao avaliacao) {
-          if (avaliacao == null) return;
-          for (Avaliacao a : avaliacoes) {
-                 if (a.getUsuario().equals(avaliacao.getUsuario())) {
-                          System.out.println("Usuário já avaliou esse jogo!");
-                          return;
-                 }
-           }
-           avaliacoes.add(avaliacao);
-} 
+        if (avaliacao == null) return;
+
+        for (Avaliacao a : avaliacoes) {
+            if (a.getUsuario().equals(avaliacao.getUsuario())) {
+                System.out.println("Usuário já avaliou esse jogo!");
+                return;
+            }
+        }
+
+        avaliacoes.add(avaliacao);
+    } 
 
     public double getMediaAvaliacoes() {
         if (avaliacoes.isEmpty()) return 0;
+
         double soma = 0;
         for (Avaliacao a : avaliacoes) {
             soma += a.getNota();
         }
+
         return soma / avaliacoes.size();
     }
 
