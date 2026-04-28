c91d8f17b89dea461bee9390091b85712d56a750
adrianagnolin <adrianaagnolin.aluno@unipampa.edu.br>
2026-04-28 15:58:10 -0300

    Update Jogo.java

--- src/Jogo.java
+++ src/Jogo.java
@@ -1,43 +1,43 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Jogo {
-    private String nome;
-    private double preco;
+public class Jogo extends Produto {
+
     private List<Avaliacao> avaliacoes = new ArrayList<>();
+    private List<DLC> dlcs = new ArrayList<>(); 
 
     public Jogo(String nome, double preco) {
-        this.nome = nome;
-        this.preco = preco;
+        super(nome, preco);
     }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public double getPreco() {
-        return preco;
-    }
+     public void adicionarDLC(DLC dlc) {
+          if (dlc != null) {
+              dlcs.add(dlc);
+          }
+     } 
 
     public void adicionarAvaliacao(Avaliacao avaliacao) {
-    for (Avaliacao a : avaliacoes) {
-        if (a.getUsuario().equals(avaliacao.getUsuario())) {
-            System.out.println("Usuário já avaliou esse jogo!");
-            return;
-        }
-    }
-    avaliacoes.add(avaliacao);
-}
+          if (avaliacao == null) return;
+          for (Avaliacao a : avaliacoes) {
+                 if (a.getUsuario().equals(avaliacao.getUsuario())) {
+                          System.out.println("Usuário já avaliou esse jogo!");
+                          return;
+                 }
+           }
+           avaliacoes.add(avaliacao);
+} 
 
     public double getMediaAvaliacoes() {
         if (avaliacoes.isEmpty()) return 0;
-    
         double soma = 0;
         for (Avaliacao a : avaliacoes) {
             soma += a.getNota();
         }
-    
         return soma / avaliacoes.size();
+    }
+
+    public List<Avaliacao> getAvaliacoes() {
+        return avaliacoes;
     }
 
     @Override
