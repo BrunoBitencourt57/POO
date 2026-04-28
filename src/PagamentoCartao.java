b5bc7f3de7143004363a173e1bbd205ef6cbc26c
adrianagnolin <adrianaagnolin.aluno@unipampa.edu.br>
2026-04-28 17:33:46 -0300

    Update PagamentoCartao.java

--- src/PagamentoCartao.java
+++ src/PagamentoCartao.java
@@ -10,12 +10,10 @@ public class PagamentoCartao implements FormaDePagamento {
     public boolean pagar(double valor) {
         if (valor <= 0) {
             throw new IllegalArgumentException("Valor inválido!");
-            return false;
         }
 
         if (numeroCartao == null || numeroCartao.isEmpty()) {
             throw new IllegalArgumentException("Cartão inválido!");
-            return false;
         }
 
         System.out.println("Pagamento de R$ " + valor + " no cartão aprovado!");
