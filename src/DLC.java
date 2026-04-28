04a6ce32f878c7e4076ff5d840452a34a57cfce7
adrianagnolin <adrianaagnolin.aluno@unipampa.edu.br>
2026-04-28 16:01:09 -0300

    DLC.java

---
+++ src/DLC.java
@@ -0,0 +1,23 @@
+public class DLC extends Produto {
+
+    private Jogo jogoBase;
+
+    public DLC(String nome, double preco, Jogo jogoBase) {
+           super(nome, preco);
+
+           if (jogoBase == null) {
+                     throw new IllegalArgumentException("Jogo base não pode ser nulo");
+           }
+
+           this.jogoBase = jogoBase;
+    } 
+
+    public Jogo getJogoBase() {
+        return jogoBase;
+    }
+
+    @Override
+    public String toString() {
+        return getNome() + " (DLC de " + jogoBase.getNome() + ") - R$ " + getPreco();
+    }
+}
