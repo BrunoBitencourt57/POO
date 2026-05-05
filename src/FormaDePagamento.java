66712ee29a0bff866906d724ba269dc3794ca202
BrunoBitencourt57 <brunomoura.aluno@unipampa.edu.br>
2026-05-05 20:19:25 -0300

    Update FormaDePagamento.java

--- src/FormaDePagamento.java
+++ src/FormaDePagamento.java
@@ -1,3 +1,14 @@
-public interface FormaDePagamento {
-    boolean pagar(double valor);
+public abstract class FormaDePagamento {
+
+    protected TipoPagamento tipo; // usa o enum
+
+    public FormaDePagamento(TipoPagamento tipo) {
+        this.tipo = tipo;
+    }
+
+    public TipoPagamento getTipo() {
+        return tipo;
+    }
+
+    public abstract boolean pagar(double valor);
 }
