d23c776134083853532d26bf95ee4a3c2dee54f2
adrianagnolin <adrianaagnolin.aluno@unipampa.edu.br>
2026-05-05 20:30:29 -0300

    Update PagamentoPix.java

--- src/PagamentoPix.java
+++ src/PagamentoPix.java
@@ -1,10 +1,14 @@
-public class PagamentoPix implements FormaDePagamento {
+public class PagamentoPix extends FormaDePagamento {
+
 
     private String chavePix;
 
+
     public PagamentoPix(String chavePix) {
+        super(TipoPagamento.PIX);
         this.chavePix = chavePix;
     }
+
 
     @Override
     public boolean pagar(double valor) {
@@ -12,11 +16,16 @@ public class PagamentoPix implements FormaDePagamento {
             throw new IllegalArgumentException("Valor inválido!");
         }
 
+
         if (chavePix == null || chavePix.isEmpty()) {
             throw new IllegalArgumentException("Chave PIX inválida!");
         }
 
+
+        System.out.println("Tipo: " + tipo);
         System.out.println("Pagamento via PIX realizado com sucesso!");
+
+
         return true;
     }
 }
