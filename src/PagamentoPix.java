19545ccf120deef4ecba4f039d8e3617257e82b0
BrunoBitencourt57 <brunomoura.aluno@unipampa.edu.br>
2026-05-19 20:04:59 -0300

    Update PagamentoPix.java

--- src/PagamentoPix.java
+++ src/PagamentoPix.java
@@ -1,14 +1,13 @@
+package pagamentos;
+
 public class PagamentoPix extends FormaDePagamento {
 
-
     private String chavePix;
-
 
     public PagamentoPix(String chavePix) {
         super(TipoPagamento.PIX);
         this.chavePix = chavePix;
     }
-
 
     @Override
     public boolean pagar(double valor) {
@@ -16,15 +15,12 @@ public class PagamentoPix extends FormaDePagamento {
             throw new IllegalArgumentException("Valor inválido!");
         }
 
-
         if (chavePix == null || chavePix.isEmpty()) {
             throw new IllegalArgumentException("Chave PIX inválida!");
         }
 
-
         System.out.println("Tipo: " + tipo);
         System.out.println("Pagamento via PIX realizado com sucesso!");
-
 
         return true;
     }
